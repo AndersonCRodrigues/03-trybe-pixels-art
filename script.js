b1761e220ada171015a06f7b9d4e341b6143ed71
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-08-31 17:29:56 -0300

    adiciona requisito 02

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function palette() {
+  const colorPalette = document.querySelector('#color-palette');
+  const colorArray = ['black', 'red', 'blue', 'green'];
+  for (let i = 0; i < colorArray.length; i += 1) {
+    const paletteDiv = document.createElement('div');
+    paletteDiv.classList = 'color';
+    paletteDiv.style.background = colorArray[i];
+    colorPalette.appendChild(paletteDiv);
+  }
+}
 
+palette();
